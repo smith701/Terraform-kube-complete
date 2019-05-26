d51220c01c5a71a93d2ade704feba06c98c63e11
smith701 <39797871+smith701@users.noreply.github.com>
2019-05-26 20:42:47 +0530

    Update connections.tf

--- cluster/connections.tf
+++ cluster/connections.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = file("../secrets/accounts/key.json")
-  project     = var.project_id
-  region      = var.region
+  project     = "${var.project_id}"
+  region      = "${var.region}"
 }
 
