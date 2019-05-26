1a4d090f6a849e51896392e138efa3de808e504f
smith701 <39797871+smith701@users.noreply.github.com>
2019-05-26 20:59:35 +0530

    Update variables.tf

--- cluster/variables.tf
+++ cluster/variables.tf
@@ -1,23 +1,23 @@
 // General Variables
 
 variable "linux_admin_username" {
-  type        = string
+  type        = "string"
   description = "User name for authentication to the Kubernetes linux agent virtual machines in the cluster."
 }
 
 variable "linux_admin_password" {
-  type        = string
+  type        = "string"
   description = "The password for the Linux admin account."
 }
 
 // GCP Variables
 variable "gcp_cluster_count" {
-  type        = string
+  type        = "string"
   description = "Count of cluster instances to start."
 }
 
 variable "project_id" {
-  type        = string
+  type        = "string"
   description = "project id name"
 }
 
@@ -28,18 +28,18 @@ variable "zone" {
 }
 
 variable "region" {
-  type        = string
+  type        = "string"
   description = "project id region"
 }
 
 variable "cluster_name" {
-  type        = string
+  type        = "string"
   description = "Cluster name for the GCP Cluster."
 }
 
 // GCP Outputs
 output "gcp_cluster_endpoint" {
-  value = google_container_cluster.gcp_kubernetes.endpoint
+  value = "ssh ${google_container_cluster.gcp_kubernetes.endpoint}"
 }
 
 output "gcp_ssh_command" {
@@ -47,6 +47,6 @@ output "gcp_ssh_command" {
 }
 
 output "gcp_cluster_name" {
-  value = google_container_cluster.gcp_kubernetes.name
+  value = "ssh ${google_container_cluster.gcp_kubernetes.name}"
 }
 
